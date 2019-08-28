0644ce2923b39db04a296b28d4ac3a4c88bf8ec5
Josh Hemsley <josh@breathehr.com>
2019-08-28 15:07:34 +0100

    account set up with 0 balance when initalized

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,14 @@
 require 'account'
 
 describe Account do
+  subject(:account) { described_class.new }
+
+  context 'users can have an account' do
+    describe '#initialization' do
+      it 'users should start with a balance of 0' do
+      expect(account.balance).to eq 0
+      end
+    end
+  end
 
 end
